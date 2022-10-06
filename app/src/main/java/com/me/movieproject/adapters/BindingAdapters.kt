316755a9f51ae193dbc135ff0d4dc7839286b5f4
Juliendy Mendes <juliendy0708@gmail.com>
2022-10-06 17:13:15 -0400

    chore: showing popular movies list on the screen (again)

--- app/src/main/java/com/me/movieproject/adapters/BindingAdapters.kt
+++ app/src/main/java/com/me/movieproject/adapters/BindingAdapters.kt
@@ -5,22 +5,33 @@ import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
-import com.me.movieproject.R
-import com.me.movieproject.api.BASE_URL
 import com.me.movieproject.model.Genre
+import com.me.movieproject.model.Movie
+import com.me.movieproject.utils.IMAGE_BASE_URL
 
-@BindingAdapter("listData")
-fun bindRecyclerView(recyclerView: RecyclerView,
+@BindingAdapter("genresList")
+fun bindGenresList(recyclerView: RecyclerView,
                      data: List<Genre>?) {
     val adapter = recyclerView.adapter as GenresListAdapter
     adapter.submitList(data)
 }
 
+@BindingAdapter("popularMoviesList")
+fun bindPopularMoviesList(recyclerView: RecyclerView,
+                     data: List<Movie>?) {
+    val adapter = recyclerView.adapter as PopularMoviesListAdapter
+    adapter.submitList(data)
+}
+
 @BindingAdapter("imageUrl")
-fun bindImage(imageView: ImageView, imgPath: String){
-    val imgUrl = BASE_URL
-    imgUrl.let {
-        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+fun bindImage(imageView: ImageView, imgPath: String?){
+
+    imgPath?.let {
+        val imgUrl = IMAGE_BASE_URL + "w200" + it
+        val imgUri = imgUrl.toUri().buildUpon().build()
         imageView.load(imgUri)
     }
+
+
+
 }
---
+++ app/src/main/java/com/me/movieproject/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.me.movieproject.utils
+
+const val BASE_URL = "https://api.themoviedb.org/3/"
+const val API_KEY = "9d24789efabca4c463fa0234a6ca34ff"
+const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
